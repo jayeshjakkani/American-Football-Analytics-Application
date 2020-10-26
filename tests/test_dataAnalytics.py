cc726b2ed2a0215f7c8039c113cff71faed97421
Kattarishabh <36841741+Kattarishabh@users.noreply.github.com>
2020-10-25 22:39:20 -0400

    Update test_dataAnalytics.py

--- tests/test_dataAnalytics.py
+++ tests/test_dataAnalytics.py
@@ -8,7 +8,7 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 
 da = dataAnalytics()
 dc = dataCollector()
-data = dc.readfile("https://github.com/jayeshjakkani/American-Football-Analytics-Application/blob/master/backend/src/NCSU.csv")
+data = dc.readfile("https://github.com/jayeshjakkani/American-Football-Analytics-Application/blob/master/backend/src/NCSU.csv?raw=true")
 ps = playsSeperator()
 all_plays = ps.getDataframesByPlays("NCST", data, "Season", "2019")
 offensive_plays = {"PUNT", "KICKOFF_RETURN", "FIELDGOAL"}
