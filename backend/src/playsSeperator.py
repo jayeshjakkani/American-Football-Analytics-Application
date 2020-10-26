c6ff182e0958e7175e4898f718deadd4301b971b
jayeshjakkani <61921635+jayeshjakkani@users.noreply.github.com>
2020-10-26 18:49:02 -0400

    Update playsSeperator.py
    Code and Style check

--- backend/src/playsSeperator.py
+++ backend/src/playsSeperator.py
@@ -3,38 +3,33 @@
 
 
 class playsSeperator:
-    
     def getDataframesByPlays(self, team, data, filtertype, filtervalue):
         if(filtertype == "Month"):
             extract = data["pff_GAMEDATE"].iloc[0]
             extract_month = extract.split("-")
             if(int(extract_month[1]) == filtervalue):
-                data = data[(data["pff_GAMEDATE"]==extract)]
+                data = data[(data["pff_GAMEDATE"] == extract)]
         elif(filtertype == "Season"):
-        	data = data[(data["pff_GAMESEASON"]==filtervalue)]
+        	data = data[(data["pff_GAMESEASON"] == filtervalue)]
         elif(filtertype == "Week"):
-        	data = data[(data["pff_WEEK"]==filtervalue)]
+            data = data[(data["pff_WEEK"] == filtervalue)]
         elif(filtertype == "Match"):
-        	data = data[(data["pff_GAMEID"]==filtervalue)]
-        elif(filtertype == "Opponent"):	
-            data1 = data[(data["pff_DEFTEAM"]==filtervalue)] 
-            data2 = data[(data["pff_OFFTEAM"]==filtervalue)]
-            data = pd.concat([data1,data2])
+            data = data[(data["pff_GAMEID"] == filtervalue)]
+        elif(filtertype == "Opponent"):
+            data1 = data[(data["pff_DEFTEAM"] == filtervalue)]
+            data2 = data[(data["pff_OFFTEAM"] == filtervalue)]
+            data = pd.concat([data1, data2])
         p = data[(data["pff_SPECIALTEAMSTYPE"] == "PUNT") & (data["pff_OFFTEAM"] == team)]
         kor = data[(data["pff_SPECIALTEAMSTYPE"] == "KICKOFF") & (data["pff_OFFTEAM"] == team)]
-        
         pr = data[(data["pff_SPECIALTEAMSTYPE"] == "PUNT") & (data["pff_DEFTEAM"] == team)]
         ko = data[(data["pff_SPECIALTEAMSTYPE"] == "KICKOFF") & (data["pff_DEFTEAM"] == team)]
-        
         epfg = data[(data["pff_SPECIALTEAMSTYPE"] == "EXTRA POINT") | (data["pff_SPECIALTEAMSTYPE"] == "FIELD GOAL")]
         fg = epfg[epfg["pff_OFFTEAM"] == team]
         fgb = epfg[epfg["pff_DEFTEAM"] == team]
-        
-        dict = {"PUNT" : p,
-                "KICKOFF_RETURN" : kor,
-                "PUNT_RETURN" : pr,
-                "KICKOFF" : ko,
-                "FIELDGOAL" : fg,
-                "FIELDGOAL_BLOCK" : fgb}
-        
+        dict = {"PUNT": p,
+                "KICKOFF_RETURN": kor,
+                "PUNT_RETURN": pr,
+                "KICKOFF": ko,
+                "FIELDGOAL": fg,
+                "FIELDGOAL_BLOCK": fgb}
         return dict
