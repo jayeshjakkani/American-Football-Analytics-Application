1edc36985f5184e5c0456c60461235bc905d2f4f
jayeshjakkani <61921635+jayeshjakkani@users.noreply.github.com>
2020-10-26 18:53:17 -0400

    Update dataCollector.py
    Code and Style check

--- backend/src/dataCollector.py
+++ backend/src/dataCollector.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-
 import pandas as pd
 
+
 class dataCollector:
-    
     def readfile(self, fileName):
         data = pd.read_csv(fileName)
         return data
