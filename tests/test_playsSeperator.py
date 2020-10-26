bc31fa6b4573f47b9f94b932076a5280f77db946
Kattarishabh <36841741+Kattarishabh@users.noreply.github.com>
2020-10-25 22:14:37 -0400

    Update test_playsSeperator.py
    Test case updated

--- tests/test_playsSeperator.py
+++ tests/test_playsSeperator.py
@@ -2,9 +2,9 @@ from backend.src.playsSeperator import playsSeperator
 from backend.src.dataCollector import dataCollector
 
 data_collector = dataCollector()
-data = data_collector.readfile("https://github.com/himol7/American-Football-Analytics-Application/blob/KrisshaJ-Testing/backend/src/NCSU.csv?raw=true")
+data = data_collector.readfile("https://github.com/jayeshjakkani/American-Football-Analytics-Application/blob/master/backend/src/NCSU.csv")
 play_separator = playsSeperator()
-plays = play_separator.getDataframesByPlays("NCST", data)
+plays = play_separator.getDataframesByPlays("NCST", data,'Season','2019')
 
 def test02():
   for k, v in plays.items():
