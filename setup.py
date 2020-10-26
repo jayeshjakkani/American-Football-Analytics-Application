9c9a80081c01ca213a6b6e635ac0b19ffd4a8d6b
Ashish Rajpurohit <46595092+PrinceNoctis12@users.noreply.github.com>
2020-10-26 16:20:06 -0400

    Create setup.py
    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+# Reference :  Setup script documentation 
+# https://setuptools.readthedocs.io/en/latest/setuptools.html
+
+from setuptools import setup, find_packages
+
+def get_requirements(filename):
+    with open(filename) as f:
+        requirements = f.read().splitlines()
+    return requirements
+
+setup(name='AFAA',
+      version='2.0',
+      description='CSC 510: Software Engineering Project 2',
+      author='Jayesh Jakkani',
+      author_email='jjakkan@ncsu.edu',
+      license="MIT",
+      packages=find_packages(),
+      python_requires=">=3.3",
+      install_requires = get_requirements("requirements.txt"),
+      include_package_data=True,
+     )
