beb836b4bc669f26547154b7e5fdf8fa594aa836
Kattarishabh <36841741+Kattarishabh@users.noreply.github.com>
2020-10-26 18:49:01 -0400

    Update afaaRunner.py
    Updated

--- backend/src/afaaRunner.py
+++ backend/src/afaaRunner.py
@@ -8,39 +8,39 @@ from dataAnalytics import dataAnalytics
 from photoImposer import photoImposer
 import pandas as pd
 
+
 class afaaRunner:
-    
-    def  __init__(self, inputFile, downloadPath, filtertype,filtervalue):
+    def __init__(self, inputFile, downloadPath, filtertype, filtervalue):
         dc = dataCollector()
         ps = playsSeperator()
         da = dataAnalytics()
         pi = photoImposer()
-        
+
         team = "NCST"
         print("download path = ")
         print(downloadPath)
-        if(len(inputFile) == 1):
+        if len(inputFile) == 1:
             data = dc.readfile(inputFile[0])
         else:
-            data=dc.readfile(inputFile[0])
-            for i in range(1,len(inputFile)):
-                df2=dc.readfile(inputFile[i])
-                data=pd.concat([data,df2])
+            data = dc.readfile(inputFile[0])
+            for i in range(1, len(inputFile)):
+                df2 = dc.readfile(inputFile[i])
+                data = pd.concat([data, df2])
         print(data)
         all_plays = ps.getDataframesByPlays(team, data, filtertype, filtervalue)
-        
+
         offensive_plays = {"PUNT", "KICKOFF_RETURN", "FIELDGOAL"}
         defensive_plays = {"PUNT_RETURN", "KICKOFF", "FIELDGOAL_BLOCK"}
-        
+
         for playType, playData in all_plays.items():
-        
+
             if playType in offensive_plays:
                 formations = playData["pff_OFFPLAYERS"]
                 ratings = playData["pff_OFFPLAYERSRATINGS"]
-                
+
             elif playType in defensive_plays:
                 formations = playData["pff_DEFPLAYERS"]
                 ratings = playData["pff_DEFPLAYERSRATINGS"]
-                
+
             countsAndRatings = da.generateTotalCountsAndRatings(formations, ratings)
             pi.imposeDataOnImage(playType, countsAndRatings, downloadPath)
